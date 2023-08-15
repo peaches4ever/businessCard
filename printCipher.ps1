8815dda822f10818e7be13ec1be24026dcad4644
peaches4ever <142324630+peaches4ever@users.noreply.github.com>
2023-08-15 17:47:35 +0000

    Update printCipher.ps1

--- printCipher.ps1
+++ printCipher.ps1
@@ -3,13 +3,18 @@ echo "####################               All your base are belong to us         
 start-sleep 1
 echo "####################        You got this far, but there's one more step       ####################"
 start-sleep 1
-echo "####################                Decrypt the following text                ####################"
+echo "####################        Decode then Decrypt the following text file       ####################"
+echo "#                                                                                                #"
 start-sleep 1
-echo ""
+echo "U2FsdGVkX1+hHfwVN48Q0zmnPJErvgtd+Y/dUXrwY91P1g0hNoGumyNY/h4IQ8YA4VMXrNL96liA"
+echo "Hh5OgJn/q/8CzIPHC3MEkIfRCVxMkyn61/NPAjyyBvdOGLaF3Pp4CLmsMVH9tReoqllL28i0q+JR"
+echo "ZgkKU6iCAVZ2DYV6FAx3/c1TjUBfnecpM51ehw1rxRbDbykthvLJU+sGvjXIFl23Ijg8zDGsceeY"
+echo "K4/QFIs="
 start-sleep 3
 echo "#                                                                                                #"
-echo "####################                            Hint:                         ####################"
+echo "####################         Hint: 256 bit AES Cipher Block Chain             ####################"
 echo "####################      The key is the name of the writer of this song:     ####################"
+echo "####################                  (lowercase, no space)                   ####################"
 echo "#                                                                                                #"
 echo "##################################################################################################"
 start-sleep 3
