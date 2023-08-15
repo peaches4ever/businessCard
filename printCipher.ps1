f90d606d88285311a6790bd03519448b5e18c2c0
peaches4ever <142324630+peaches4ever@users.noreply.github.com>
2023-08-15 12:14:38 -0400

    Update printCipher.ps1

--- printCipher.ps1
+++ printCipher.ps1
@@ -1,43 +1,37 @@
-echo "hello world"
+echo "##################################################################################################"
+echo "####################               All your base are belong to us             ####################"
+start-sleep 1
+echo "# youve been hacked #"
+start-sleep 1
+echo "# why did you run this script #"
+start-sleep 1
+echo "# don't do that again #"
+start-sleep 5
+echo "# just kidding this script does nothing... #"
+start-sleep 5
+echo "# have a song #"
+echo "#  #"
+echo "##################################################################################################"
+start-sleep 1
 
 
 start-job {
-
 [console]::beep(440,500)
-
 [console]::beep(440,500)
-
 [console]::beep(440,500)
-
 [console]::beep(349,350)
-
 [console]::beep(523,150)
-
 [console]::beep(440,500)
-
 [console]::beep(349,350)
-
 [console]::beep(523,150)
-
 [console]::beep(440,1000)
-
 [console]::beep(659,500)
-
 [console]::beep(659,500)
-
 [console]::beep(659,500)
-
 [console]::beep(698,350)
-
 [console]::beep(523,150)
-
 [console]::beep(415,500)
-
 [console]::beep(349,350)
-
 [console]::beep(523,150)
-
 [console]::beep(440,1000)
-
-
 ﻿}
